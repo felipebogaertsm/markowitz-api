ae9f593324661b8482795c5f1976787250527b69
Felipe Bogaerts de Mattos <me@felipebm.com>
2025-05-29 10:54:13 -0300

    Deleted old api

--- api/optimizer/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-
-
-class PortfolioOptimizationSerializer(serializers.Serializer):
-    tickers = serializers.CharField(
-        help_text="Enter comma-separated ticker symbols."
-    )
-    target_return = serializers.FloatField()
